6b78cda288992d3fdb5073ec34034017354057d9
James Carpenter <James Carpenter@10.162.10.87>
2018-03-25 16:14:47 -0500

    First Start

---
+++ Snake/src/Draw.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Point;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Draw extends JPanel {
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, 800, 800);
+		Snake snake = Snake.snake;
+		g.setColor(Color.GREEN);
+		for (Point point : snake.body) {
+			g.fillRect(point.x * Snake.SCALE, point.y * Snake.SCALE, Snake.SCALE, Snake.SCALE);
+		}
+		g.fillRect(snake.head.x * Snake.SCALE, snake.head.y * Snake.SCALE, Snake.SCALE, Snake.SCALE);
+		g.setColor(Color.RED);
+		g.fillRect(snake.apple.x * Snake.SCALE, snake.apple.y * Snake.SCALE, Snake.SCALE, Snake.SCALE);
+		String s="Score: " + snake.score;
+		g.setColor(Color.WHITE);
+		g.setFont(new Font("TimesRoman", Font.PLAIN, 16));
+		g.drawString(s, (int) snake.jframe.getWidth() / 2 - s.length() / 2, 15);
+	} 
+}
---
+++ Snake/src/Snake.java
@@ -0,0 +1,178 @@
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class Snake implements ActionListener, KeyListener{
+	public static Snake snake;
+	public Draw draw;
+	public JFrame jframe;
+	public Random random;
+	public Timer timer;
+	public boolean gameOver = false;
+	public ArrayList<Point> body = new ArrayList<Point>();
+	public int count = 0, score = 0, tailLength = 5;
+	public static final int UP = 0, DOWN = 1, RIGHT = 2, LEFT = 3, SCALE=10;
+	public int direction = DOWN;
+	public Point head, apple;
+
+	public Snake() {
+		jframe = new JFrame("Snake");
+		draw = new Draw();
+		timer = new Timer(20, this);
+		jframe.setVisible(true);
+		jframe.setSize(800, 800);
+		jframe.setResizable(false);
+		jframe.setLocation(550, 125);
+
+		jframe.add(draw);
+		jframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		jframe.addKeyListener(this);
+		game();
+		
+	}
+	
+	public void game()
+	{
+		gameOver=false;
+		score=0;
+		tailLength=5;
+		direction=DOWN;
+		head = new Point(0,-1);
+		random = new Random();
+		body.clear();
+		apple = new Point(random.nextInt(78), random.nextInt(76));
+		
+		for(int i=0; i<tailLength; i++)
+		{
+			body.add(new Point(head.x, head.y));
+		}
+		timer.start();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		draw.repaint();
+		count++;
+		if (count % 3 == 0 && head != null && gameOver != true) {
+			body.add(new Point(head.x, head.y));
+			if (direction == UP) {
+				if (head.y - 1 >= 0 && hitSelf(head.x, head.y - 1)) {
+					head = new Point(head.x, head.y - 1);
+				} else {
+					gameOver = true;
+				}
+
+			}
+			if (direction == DOWN) {
+				if (head.y + 1 <=76 && hitSelf(head.x, head.y + 1)) {
+					head=new Point(head.x, head.y + 1);
+				} else {
+					gameOver = true;
+				}
+			}
+			if (direction == RIGHT) {
+				if (head.x + 1 <= 78.5 && hitSelf(head.x + 1, head.y)) {
+					head=new Point(head.x + 1, head.y);
+				} else {
+					gameOver = true;
+				}
+			}
+			if (direction == LEFT) {
+				if (head.x - 1 >= 0 && hitSelf(head.x - 1, head.y)) {
+					head=new Point(head.x - 1, head.y);
+				} else {
+					gameOver = true;
+				}
+			}
+			if(body.size() > tailLength)
+			{
+				body.remove(0);
+			}
+		
+			if (apple != null) {
+				if (head.x == apple.x && head.y == apple.y) {
+					
+					score+=10;
+					if(score==100)
+					{
+						gameOver=true;
+					}
+					else
+					{
+					apple.setLocation(random.nextInt(78), random.nextInt(76));
+					}
+					tailLength+=2;
+					
+				}
+			}
+		}
+		
+
+	}
+
+	public static void main(String[] args) {
+		snake=new Snake();
+	}
+	
+	public boolean hitSelf(int x, int y)
+	{
+		for(Point point: body)
+		{
+			if(point.equals(new Point(x, y)))
+			{
+				return false;
+			}
+				
+		}
+		return true;
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int i=e.getKeyCode();
+		if(i==KeyEvent.VK_A && direction != RIGHT)
+		{
+			direction = LEFT;
+		}
+		if(i==KeyEvent.VK_D && direction != LEFT)
+		{
+			direction = RIGHT;
+		}
+		if(i==KeyEvent.VK_S && direction != UP)
+		{
+			direction = DOWN;
+		}
+		if(i==KeyEvent.VK_W && direction != DOWN)
+		{  
+			direction = UP;
+		}
+		if(i==KeyEvent.VK_SPACE && gameOver==true)
+		{
+			game();
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
